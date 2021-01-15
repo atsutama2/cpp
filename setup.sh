2c237533067089c25bcc148d927b9750ae9fd6b2
yuyafujita-eng <yuyafujita.eng@gmail.com>
2021-01-15 20:12:39 +0900

    通常設定も追加

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@
 git config --local user.name "yuyafujita-eng"
 git config --local user.email "yuyafujita.eng@gmail.com"
 git remote set-url origin git@yuyafujita-eng-github:yuyafujita-eng/cpp-study.git
+#git remote set-url origin git@github.com:yuyafujita-eng/cpp-study.git
