a3592a541ae110c739aa75576db2d12cb02c1598
yuyafujita-eng <yuyafujita.eng@gmail.com>
2021-01-18 19:05:56 +0900

    C++ 学習

---
+++ beginners/EX4/ex4.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // 一年の秒数
+    int seconds = 365 * 24 * 60 * 60;
+    int year_for2 = seconds * 2;
+    int year_for5 = seconds * 5;
+    int year_for10 = seconds * 10;
+
+    // 以下のコメント/* */を消して追記する
+    cout << seconds << endl;
+    cout << year_for2 << endl;
+    cout << year_for5 << endl;
+    cout << year_for10 << endl;
+}
