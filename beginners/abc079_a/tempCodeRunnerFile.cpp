f80c70c1cbce7e88a7e9df2397ca95d60fb9fd03
yuyafujita-eng <yuyafujita.eng@gmail.com>
2021-01-20 18:28:12 +0900

    学習

--- beginners/abc079_a/tempCodeRunnerFile.cpp
+++ beginners/abc079_a/tempCodeRunnerFile.cpp
@@ -6,10 +6,7 @@ int main() {
     int n;
     cin >> n;
 
-    string str = to_string(n);
-
-    if (str[0] == str[1] && str[1] == str[2] ||
-        str[1] == str[3] && str[2] == str[3]) {
+    if (n % 1111 == 0) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
