2a60cc342bb729f987bd1630df67f4c3f204e8d3
atsutama2 <atsutama.0713@gmail.com>
2021-02-01 18:59:01 +0900

    git設定変更

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #git設定
-git config --local user.name "yuyafujita-eng"
-git config --local user.email "yuyafujita.eng@gmail.com"
+git config --local user.name "atsutama2"
+git config --local user.email "atsutama.0713@gmail.com"
 git remote set-url origin git@yuyafujita-eng-github:yuyafujita-eng/cpp-study.git
 #git remote set-url origin git@github.com:yuyafujita-eng/cpp-study.git
